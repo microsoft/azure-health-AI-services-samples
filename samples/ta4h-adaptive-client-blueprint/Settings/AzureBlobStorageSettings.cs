5bb13255508d7cb4ce35f5ac9e9fce0911f55301
udnaveh <udnaveh@microsoft.com>
2023-06-20 15:52:14 +0300

    add doctrings

--- samples/ta4h-adaptive-client-blueprint/Settings/AzureBlobStorageSettings.cs
+++ samples/ta4h-adaptive-client-blueprint/Settings/AzureBlobStorageSettings.cs
@@ -1,7 +1,13 @@
 ﻿public class AzureBlobStorageSettings
 {
+    /// <summary>
+    /// Connection string fot Azure Storage
+    /// </summary>
     public string ConnectionString { get; set; }
 
+    /// <summary>
+    /// The name of blob container where the documents are stored
+    /// </summary>
     public string ContainerName { get; set; }
 }
 
--- samples/ta4h-adaptive-client-blueprint/Settings/DataProcessingOptions.cs
+++ samples/ta4h-adaptive-client-blueprint/Settings/DataProcessingOptions.cs
@@ -1,10 +1,25 @@
 ﻿public class DataProcessingOptions
 {
-    public int RepeatTimes { get; set; } = 1;
-
+    /// <summary>
+    /// The initial maximum number of current jobs that can be sent to text analytics.
+    /// </summary>
     public int InitialQueueSize { get; set; } = 20;
 
+    /// <summary>
+    /// If true, the order of the documents set for processing will be random.
+    /// </summary>
     public bool Shuffle { get; set; } = false;
+
+    /// <summary>
+    /// The maximun number of documents to be processed by the application.
+    /// </summary>
+    public int MaxDocs { get; set; }
+
+    /// <summary>
+    /// For dev purposes only - the application will cycle to the documents this number of times.
+    /// </summary>
+    public int RepeatTimes { get; set; } = 1;
+
 }
 
 
