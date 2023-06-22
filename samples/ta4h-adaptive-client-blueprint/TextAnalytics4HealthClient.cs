b5386bda632bfc53f9c1f31e20f45cc46e5689f3
udnaveh <udnaveh@microsoft.com>
2023-06-22 13:36:42 +0300

    ta4hclient wip

--- samples/ta4h-adaptive-client-blueprint/TextAnalytics4HealthClient.cs
+++ samples/ta4h-adaptive-client-blueprint/TextAnalytics4HealthClient.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
+using System.Diagnostics;
 using System.Net;
 using System.Text;
 using TextAnalyticsHealthcareAdaptiveClient.TextAnalyticsApiSchema;
@@ -11,6 +12,9 @@ public class TextAnalytics4HealthClient
     private readonly HttpClient _httpClient;
     private readonly ILogger _logger;
     private readonly Ta4hOptions _options;
+
+    private const string ApiKeyHeaderName = "Ocp-Apim-Subscription-Key"; // "apim-subscription-id"; //
+    private const string OperationLocationHeaderName = "Operation-Location";
 
     public TextAnalytics4HealthClient(ILogger<TextAnalytics4HealthClient> logger, IOptions<Ta4hOptions> options)
     {
@@ -47,9 +51,9 @@ public class TextAnalytics4HealthClient
         {
             Content = new StringContent(serializedRequestBody, Encoding.UTF8, "application/json")
         };
-        request.Headers.Add("Ocp-Apim-Subscription-Key", _options.ApiKey);
+        request.Headers.Add(ApiKeyHeaderName, _options.ApiKey);
         var response = await SendRequestWithRetryMechanismAsync(request);
-        response.Headers.TryGetValues("Operation-Location", out var values);
+        response.Headers.TryGetValues(OperationLocationHeaderName, out var values);
         var operationLocation = values.FirstOrDefault();
         var jobId = operationLocation.Split("?")[0].Split("/").Last();
         payload.JobId = jobId;
@@ -60,7 +64,7 @@ public class TextAnalytics4HealthClient
     {
         var url = $"/language/analyze-text/jobs/{jobId}?api-version={_options.ApiVersion}&top={_options.MaxDocsPerRequest}";
         var request = new HttpRequestMessage(HttpMethod.Get, url);
-        request.Headers.Add("Ocp-Apim-Subscription-Key", _options.ApiKey);
+        request.Headers.Add(ApiKeyHeaderName, _options.ApiKey);
         var response = await SendRequestWithRetryMechanismAsync(request);
         var content = await response.Content.ReadAsStringAsync();
         return JsonConvert.DeserializeObject<TextAnlyticsJobResponse>(content);
@@ -75,10 +79,12 @@ public class TextAnalytics4HealthClient
         {
             try
             {
+                _logger.LogDebug("attempt number {attempt}, {uri}", attemptsCounter + 1, request.RequestUri);
                 var response = await _httpClient.SendAsync(request);
                 if (!response.IsSuccessStatusCode)
                 {
                     var errorContent = await response.Content?.ReadAsStringAsync();
+                    _logger.LogWarning("attempt number {attempt}, {uri}, Got an http excpetion, retry-after header = {RetryAfter}, {statusCode}, {err}", attemptsCounter + 1, request.RequestUri, response.Headers.RetryAfter, response.StatusCode, errorContent);
                     _logger.LogWarning("Got an http excpetion, retry-after header = {RetryAfter}", response.Headers.RetryAfter);
                     throw new HttpException(response.StatusCode, errorContent);
                 }
@@ -88,9 +94,9 @@ public class TextAnalytics4HealthClient
             {
                 lastException = httpException;
                 var jitterer = new Random(request.RequestUri.OriginalString.GetHashCode());
-                var tryAgainInSeconds = jitterer.Next(15, 60);
+                var tryAgainInSeconds = jitterer.Next(15, 30);
                 _logger.LogWarning("Request to {url} failed with http 429 status. next retry in {tryAgainInSeconds} seconds", request.RequestUri, tryAgainInSeconds);
-                await Task.Delay(TimeSpan.FromSeconds(1));
+                await Task.Delay(TimeSpan.FromSeconds(tryAgainInSeconds));
                 _logger.LogInformation("Waited to retry for {tryAgainInSeconds}, ready to try again", tryAgainInSeconds);
             }
             catch (TaskCanceledException taskCancelledException)
@@ -100,12 +106,16 @@ public class TextAnalytics4HealthClient
                 _logger.LogWarning("Request to {url} failed with timeout: {message}. next retry in {tryAgainInSeconds}", request.RequestUri, taskCancelledException.Message, tryAgainInSeconds);
                 await Task.Delay(TimeSpan.FromSeconds(tryAgainInSeconds));
             }
+            catch (Exception ex) 
+            {
+                _logger.LogError(ex, "Unexpected error");
+            }
             var newRequest = new HttpRequestMessage(request.Method, request.RequestUri)
             {
                 Content = request.Content,
                 
             };
-            newRequest.Headers.Add("Ocp-Apim-Subscription-Key", _options.ApiKey);
+            newRequest.Headers.Add(ApiKeyHeaderName, _options.ApiKey);
             request = newRequest;
         }
         throw lastException;
